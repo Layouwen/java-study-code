1e7a4825e0603006678ba61d65d1905f7460ff8f
Layouwen <layouwen@gmail.com>
2020-11-05 15:45:55 +0800

    sizeBig

--- 20201105/helloWorld.java
+++ 20201105/helloWorld.java
@@ -1,4 +1,4 @@
-public class hello {
+public class helloWorld {
   public static void main(String[] args) {
     int a = 1 + 'a';
     System.out.println(a);
---
+++ 20201105/sizeBig.java
@@ -0,0 +1,7 @@
+public class sizeBig {
+  public static void main(String[] args) {
+    int one = 100;
+    int two = 200;
+    System.out.println(one > two ? '是' : "不是");
+  }
+}
