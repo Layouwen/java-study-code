47fce0c141e606b3adde1e62054c8717c3aa2ac9
Layouwen <layouwen@gmail.com>
2020-11-05 16:15:46 +0800

    scannerDemo

---
+++ 20201105/scannerDemo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class scannerDemo {
+  public static void main(String[] args) {
+    Scanner so = new Scanner(System.in);
+    int i = so.nextInt();
+    System.out.println(i);
+  }
+}
