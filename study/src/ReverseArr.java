2f0e2ef4f2a0b6b4b32173a14687b4d9586ab4f8
liangyouwen <layouwen@gmail.com>
2020-11-08 18:06:19 +0800

    remote newArr

--- study/src/ReverseArr.java
+++ study/src/ReverseArr.java
@@ -1,7 +1,6 @@
 public class ReverseArr {
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 4, 5};
-    int[] newArr = new int[arr.length];
     for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
       int temp = arr[i];
       arr[i] = arr[j];
