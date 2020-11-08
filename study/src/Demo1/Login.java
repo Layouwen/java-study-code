7f6f5bff148546aa0a4dd11a3b9317fbca4985b1
liangyouwen <layouwen@gmail.com>
2020-11-08 20:01:11 +0800

    Login

---
+++ study/src/Demo1/Login.java
@@ -0,0 +1,28 @@
+package Demo1;
+
+import java.util.Scanner;
+
+public class Login {
+  public static void main(String[] args) {
+    String name = "layouwen";
+    String pwd = "123";
+
+    for (int i = 0; i < 3; i++) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("请输入你的用户名：");
+      String inputName = sc.nextLine();
+      System.out.println("请输入你的密码：");
+      String inputPwd = sc.nextLine();
+      if (inputName.equals(name) && inputPwd.equals(pwd)) {
+        System.out.println("登录成功");
+        break;
+      } else {
+        if (i == 2) {
+          System.out.println("登录失败,请联系管理员重置密码！");
+        } else {
+          System.out.println("密码错误，你还有" + (2 - i) + "机会");
+        }
+      }
+    }
+  }
+}
