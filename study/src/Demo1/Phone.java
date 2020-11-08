f2fc0f4a1597ce6a4257ed47eb7a3d49644c4f00
liangyouwen <layouwen@gmail.com>
2020-11-08 19:49:42 +0800

    Phone Class

---
+++ study/src/Demo1/Phone.java
@@ -0,0 +1,5 @@
+package Demo1;
+
+public class Phone {
+  int price;
+}
---
+++ study/src/Demo1/PhoneDemo.java
@@ -0,0 +1,8 @@
+package Demo1;
+
+public class PhoneDemo {
+  public static void main(String[] args) {
+    Phone p = new Phone();
+    System.out.println(p.price);
+  }
+}
