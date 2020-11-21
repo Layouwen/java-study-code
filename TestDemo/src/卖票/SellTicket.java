835d7ae1d9c5cf3b01a4368113c3a7fb159d945d
liangyouwen <layouwen@gmail.com>
2020-11-22 03:04:58 +0800

    卖票

--- TestDemo/src/卖票/SellTicket.java
+++ TestDemo/src/卖票/SellTicket.java
@@ -2,13 +2,21 @@ package 卖票;
 
 public class SellTicket implements Runnable {
   private int tickets = 100;
+  private Object obj = new Object();
 
   @Override
   public void run() {
     while (true) {
       if (tickets > 0) {
-        System.out.println(Thread.currentThread().getName() + "正在出售第" + tickets + "张票");
-        tickets--;
+        synchronized (obj) {
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          System.out.println(Thread.currentThread().getName() + "正在出售第" + tickets + "张票");
+          tickets--;
+        }
       }
     }
   }
