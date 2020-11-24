890b67671169158ca7814b7e39ef7eb006c3811c
liangyouwen <layouwen@gmail.com>
2020-11-24 18:30:43 +0800

    TCP通信程序练习5

---
+++ TestDemo/src/TCP通信程序练习5/ClientDemo.java
@@ -0,0 +1,37 @@
+package TCP通信程序练习5;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.Buffer;
+
+/*
+ *  客户端：数据来自文件，接收服务器反馈
+ *  服务器：接收到的数据写入文本文件，给出反馈
+ */
+public class ClientDemo {
+  public static void main(String[] args) throws IOException {
+    Socket s = new Socket("127.0.0.1", 10244);
+
+    BufferedReader br = new BufferedReader(new FileReader("./student-demo.txt"));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+    String line;
+    while ((line = br.readLine()) != null) {
+      bw.write(line);
+      bw.newLine();
+      bw.flush();
+    }
+
+    bw.write("886");
+    bw.newLine();
+    bw.flush();
+
+
+    BufferedReader brClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    String data = brClient.readLine();
+    System.out.println("服务器反馈是：" + data);
+
+    br.close();
+    s.close();
+  }
+}
---
+++ TestDemo/src/TCP通信程序练习5/ServerDemo.java
@@ -0,0 +1,34 @@
+package TCP通信程序练习5;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerDemo {
+  public static void main(String[] args) throws IOException {
+    ServerSocket ss = new ServerSocket(10244);
+
+    Socket s = ss.accept();
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    BufferedWriter bw = new BufferedWriter(new FileWriter("./TCPDemo5.txt"));
+
+    String line;
+    while ((line = br.readLine()) != null) {
+      if ("886".equals(line)) {
+        break;
+      }
+      bw.write(line);
+      bw.newLine();
+      bw.flush();
+    }
+
+    BufferedWriter bwServer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+    bwServer.write("文件上传成功");
+    bwServer.newLine();
+    bwServer.flush();
+
+    bw.close();
+    ss.close();
+  }
+}
